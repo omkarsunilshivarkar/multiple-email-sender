af310a2ac31f3e6d79698e1873ebf35b86a3b397
Omkar Shivarkar <omkarsunilshivarkar@gmail.com>
2023-12-09 12:05:41 +0530

    Writing comments to execute the program.

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import smtplib as s
 ob=s.SMTP('smtp.gmail.com',587)
 ob.ehlo()
 ob.starttls()
-ob.login('your gmail id','your gmail app password')
+ob.login('sender gmail id','sender gmail app password')
 subject="test python"
 body="i love python"
 message="subject:{}\n\n{}".format(subject,body)
-listadd=['omkarsunilshivarkar@gmail.com','shivarkarsunilomkar@gmail.com']
-ob.sendmail('omkarsunilshivarkar@gmail.com',listadd,message)
+listadd=['receiver gmail address 1','receiver gmail address 2']
+ob.sendmail('sender gmail address',listadd,message)
 print('mail sent')
 ob.quit()
